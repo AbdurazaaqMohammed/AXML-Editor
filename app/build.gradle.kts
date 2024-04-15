1047cc44675ed48b5203f2955bed6785114f8866
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-04-15 10:41:15 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.abdurazaaqmohammed.androidmanifesteditor"
         minSdk = 16
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
     }
 
