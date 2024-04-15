25ae54c19d3478e005511ff56af636509c690e33
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-04-14 20:35:02 -0400

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.abdurazaaqmohammed.androidmanifesteditor"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.abdurazaaqmohammed.androidmanifesteditor"
+        minSdk = 16
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = false
+    }
+}
+dependencies {
+    implementation("commons-io:commons-io:2.11.0")
+    testImplementation("junit:junit:4.13.2")
+}
